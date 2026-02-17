f86bd338946b594390155df844ffd2aed7cd43b4
OscarLeonel16 <oscaribanesgarcia8@gmail.com>
2026-02-16 23:34:46 -0500

    Update player.cpp

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -2907,7 +2907,7 @@ void CBasePlayer::Spawn( void )
 {
 	m_flStartCharge = gpGlobals->time;
 	pev->classname = MAKE_STRING( "player" );
-	pev->health = 100;
+	pev->health = 200;
 	pev->armorvalue = 0;
 	pev->takedamage = DAMAGE_AIM;
 	pev->solid = SOLID_SLIDEBOX;
